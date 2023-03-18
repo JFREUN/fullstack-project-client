f74bc91380ca59dced3d6a1151db49360faa7b47
Sirah Tabarez <sartab2011@gmail.com>
2023-03-18 16:48:25 +0100

    components and pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function HomePage() {
+    return (
+        
+      <div>
+        <Navbar/>
+        <h1>Bienvenue Dyner!</h1>
+        <img src="" alt="" />
+        <button>Sign Up</button>
+        <button>Login</button>
+        <Footer/>
+      </div>
+    );
+  }
+  
+  export default HomePage;
